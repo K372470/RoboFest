407304a2fc323b65bfa3e2571b212240aec10852
Keshka <74170358+K372470@users.noreply.github.com>
2025-02-12 01:58:47 +0300

    Delete EspColorSensor directory
    This solution is not needed anymore

--- EspColorSensor/src/camera-functions/camera_algorithms.h
+++
@@ -1,7 +0,0 @@
-#include <esp_err.h>
-#include <colors/HslColor.h>
-#define CAMERA_TAG "Thinker"
-
-esp_err_t getColorOfObject(lego_color_t &result);
-esp_err_t getFastLine(lego_color_t &result);
-esp_err_t getLines(lego_color_t* result,size_t &lenght);
--- EspColorSensor/src/comunication/ev3_command.h
+++
@@ -1,6 +0,0 @@
-typedef enum
-{
-  COMMAND_NONE,
-  COMMAND_GET_COLOR,
-  COMMAND_GET_LINES,
-} ev3_command_t;
